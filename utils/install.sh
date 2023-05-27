964b57b57f7423e96a39653c6989bd0c110e523a
Nicolò Diamante <hello@nicolodiamante.com>
2023-05-27 15:08:47 +0200

    improved OS version detection

--- utils/install.sh
+++ utils/install.sh
@@ -6,8 +6,9 @@
 
 # Detect the OS if darwin then determines the OS version
 # (from macOS 12.6.1 Monterey or later).
-OS_VERS="$(uname -r)"
-[[ "$OSTYPE" = darwin* && "$OS_VERS" > 20 ]] || exit 1
+SW_VERS=$(sw_vers -buildVersion)
+OS_VERS=$(sed -E -e 's/([0-9]{2}).*/\1/' <<< "$SW_VERS")
+[[ "$OSTYPE" = darwin* && "$OS_VERS" -ge 21 ]] || exit 1
 
 # Defines the PATHs.
 LIB_AGENTS="${HOME}/Library/LaunchAgents"
