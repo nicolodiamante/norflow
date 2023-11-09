b0995d1560157ac47338dad7228f422db966def9
Nicolò Diamante <hello@nicolodiamante.com>
2023-11-09 17:49:37 +0100

    refined bootstrap, install/uninstall scripts with enhanced system compatibility and agent handling

--- utils/install.sh
+++ utils/install.sh
@@ -4,13 +4,13 @@
 # Install Norflow
 #
 
-# Validate OS and version
+# Validate OS and version.
 if [[ "$OSTYPE" != "darwin"* ]]; then
   echo "Error: This script is only compatible with macOS."
   exit 1
 fi
 
-# Validate macOS version
+# Validate macOS version.
 SW_VERS=$(sw_vers -buildVersion)
 OS_VERS=$(sed -E -e 's/([0-9]{2}).*/\1/' <<<"$SW_VERS")
 if [[ "$OS_VERS" -lt 21 ]]; then
@@ -18,17 +18,21 @@ if [[ "$OS_VERS" -lt 21 ]]; then
   exit 1
 fi
 
-# Define paths
+# Defines the PATHs.
 LIB_AGENTS="${HOME}/Library/LaunchAgents"
 AGENT_SOURCE="${HOME}/norflow/agent/com.shell.Norflow.plist"
 AGENT_TARGET="${LIB_AGENTS}/com.shell.Norflow.plist"
 
-# Ensure the LaunchAgents directory exists
-if [[ ! -d "$LIB_AGENTS" ]]; then
-  mkdir -p "${LIB_AGENTS}" || { echo "Failed to create LaunchAgents directory." >&2; exit 1; }
+# Ensure the LaunchAgents directory exists.
+if ! [[ -d "$LIB_AGENTS" ]]; then
+  echo "Creating LaunchAgents directory..."
+  if ! mkdir -p "${LIB_AGENTS}"; then
+    echo "Failed to create LaunchAgents directory." >&2
+    exit 1
+  fi
 fi
 
-# Create or update the symbolic link for the agent
+# Create or update the symbolic link for the agent.
 if ln -sf "${AGENT_SOURCE}" "${AGENT_TARGET}"; then
   echo "Symbolic link created/updated for Norflow agent."
 else
@@ -36,7 +40,7 @@ else
   exit 1
 fi
 
-# Load the agent if the source exists
+# Load the agent if the source exists.
 if [[ -f "$AGENT_SOURCE" ]]; then
   if launchctl load "${AGENT_TARGET}"; then
     echo "Norflow agent loaded successfully."
--- utils/uninstall.sh
+++ utils/uninstall.sh
@@ -4,23 +4,31 @@
 # Uninstall Norflow
 #
 
-# Define paths
+# Defines the PATHs.
 LIB_AGENTS="${HOME}/Library/LaunchAgents"
 AGENT="${LIB_AGENTS}/com.shell.Norflow.plist"
 
 # Check if the file exists and is a symlink before attempting to remove.
 if [[ -L "${AGENT}" ]]; then
+  echo "Removing the agent symlink..."
   # Unload the agent if it was loaded.
-  if launchctl list | grep -q "$(basename "${AGENT}" .plist)"; then
-    launchctl unload "${AGENT}" 2>/dev/null && echo "Agent unloaded successfully." || { echo "Failed to unload the agent."; exit 1; }
+  agent_name=$(basename "${AGENT}" .plist)
+  if launchctl list | grep -q "${agent_name}"; then
+    echo "Unloading the agent..."
+    if ! launchctl unload "${AGENT}" 2>/dev/null; then
+      echo "Failed to unload the agent." >&2
+      exit 1
+    else
+      echo "Agent unloaded successfully."
+    fi
   fi
 
   # Remove the symlink.
-  if rm -f "${AGENT}"; then
-    echo "Successfully removed the agent symlink."
-  else
+  if ! rm -f "${AGENT}"; then
     echo "Failed to remove the agent symlink." >&2
     exit 1
+  else
+    echo "Successfully removed the agent symlink."
   fi
 elif [[ -f "${AGENT}" ]]; then
   echo "The agent file exists but is not a symlink. Manual removal required." >&2
